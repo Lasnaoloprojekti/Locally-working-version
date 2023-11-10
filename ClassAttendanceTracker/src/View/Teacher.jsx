390db1b0291bbab42aa8e9bb749be8d22cc10b10
matiasnaakka <matias5230@gmail.com>
2023-11-10 14:05:35 +0200

    all of the other files

--- ClassAttendanceTracker/src/View/Teacher.jsx
+++ ClassAttendanceTracker/src/View/Teacher.jsx
@@ -1,34 +1,46 @@
+import React, { useState, useContext } from "react";
 import logo from "../assets/logo.png";
 import { useNavigate } from "react-router-dom";
-import { useContext } from "react";
 import { userContext } from "../context/userContext";
 import AddStudents from "../components/AddStudents";
 import CreateCourse from "../components/CreateCourse";
 import SelectCourse from "../components/SelectCourse";
-import CourseDelete from "../components/CourseDelete";
+import CourseDelete from "../components/CourseDelete"; // Import CourseDelete component
 
-
-export const TeacherHome = () => {
+const TeacherHome = () => {
   const navigate = useNavigate();
   const { userInfo, setUserInfo } = useContext(userContext);
+  const [activeComponent, setActiveComponent] = useState("createCourse"); // Initial active component
 
   const handleLogout = () => {
-
     localStorage.removeItem("token");
-
     setUserInfo({
       staff: false,
       firstname: "",
       lastname: "",
     });
-
     navigate("/login");
+  };
+
+  const renderComponent = () => {
+    switch (activeComponent) {
+      case "createCourse":
+        return <CreateCourse />;
+      case "addStudents":
+        return <AddStudents />;
+      case "selectCourse":
+        return <SelectCourse />;
+      case "deleteCourse": // New case for CourseDelete
+        return <CourseDelete />;
+      default:
+        return null;
+    }
   };
 
   return (
     <>
       <nav className="flex justify-between items-center">
-        <img className=" h-24 p-3" src={logo} alt="Logo" />
+        <img className="h-24 p-3" src={logo} alt="Logo" />
         <ul className="flex items-center">
           <li className="text-2xl ml-2">
             Welcome, {userInfo.firstname} {userInfo.lastname}
@@ -41,10 +53,41 @@ export const TeacherHome = () => {
         </ul>
       </nav>
       <div className="flex flex-col items-center">
-        <CreateCourse />
-        <AddStudents />
-        <CourseDelete/>
-        <SelectCourse />
+        <div className="flex mb-4">
+          <button
+            onClick={() => setActiveComponent("createCourse")}
+            className={`${activeComponent === "createCourse"
+              ? "bg-orange-600 text-white"
+              : "bg-gray-300"
+              } px-4 py-2 rounded mr-2`}>
+            Create Course
+          </button>
+          <button
+            onClick={() => setActiveComponent("addStudents")}
+            className={`${activeComponent === "addStudents"
+              ? "bg-orange-600 text-white"
+              : "bg-gray-300"
+              } px-4 py-2 rounded mr-2`}>
+            Add Students
+          </button>
+          <button
+            onClick={() => setActiveComponent("selectCourse")}
+            className={`${activeComponent === "selectCourse"
+              ? "bg-orange-600 text-white"
+              : "bg-gray-300"
+              } px-4 py-2 rounded mr-2`}>
+            Select Course
+          </button>
+          <button
+            onClick={() => setActiveComponent("deleteCourse")}
+            className={`${activeComponent === "deleteCourse"
+              ? "bg-orange-600 text-white"
+              : "bg-gray-300"
+              } px-4 py-2 rounded`}>
+            Delete Course
+          </button>
+        </div>
+        {renderComponent()}
       </div>
     </>
   );
---
+++ ClassAttendanceTracker/src/components/OpenAttendance.jsx
@@ -0,0 +1,115 @@
+import React, { useState } from "react";
+import createCourse from "../Hooks/createApiHooks";
+
+const CreateCourse = () => {
+
+    const [courseData, setCourseData] = useState({
+        courseName: '',
+        groupName: '',
+        topics: [],
+        startDate: '',
+        endDate: ''
+    });
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const requestData = {
+                courseName: courseData.courseName,
+                groupName: courseData.groupName,
+                topics: courseData.topics,
+                startDate: courseData.startDate,
+                endDate: courseData.endDate,
+            };
+
+            const response = await createCourse(requestData);
+        } catch (error) {
+            console.error("Error creating course:", error);
+        }
+    };
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        console.log(`Updating ${name} with value: ${value}`);
+
+        if (name === 'topics') {
+            // Split the input value by commas and create an array of trimmed topics
+            const topicsArray = value.split(',').map(topic => topic.trim());
+            setCourseData((prevData) => ({
+                ...prevData,
+                [name]: topicsArray,
+            }));
+        } else {
+            // For other fields, update the state normally
+            setCourseData((prevData) => ({
+                ...prevData,
+                [name]: value,
+            }));
+        }
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+
+            <div className="mb-4">
+                <label className="block text-black text-sm font-semibold mb-2">Course name</label>
+                <input
+                    required
+                    className="w-full p-2 text-black border rounded"
+                    type="text"
+                    placeholder="Enter course name"
+                    name='courseName'
+                    onChange={handleChange}
+                />
+            </div>
+            <div className="mb-4">
+                <label className="block text-black text-sm font-semibold mb-2">Group name</label>
+                <input
+                    required
+                    className="w-full p-2 text-black border rounded"
+                    type="text"
+                    placeholder="Enter Group name"
+                    name='groupName'
+                    onChange={handleChange}
+                />
+            </div>
+
+            <div className="mb-4">
+                <label className="block text-black text-sm font-semibold mb-2">Topics</label>
+                <input
+                    required className="w-full p-2 text-black border rounded"
+                    type="text"
+                    placeholder="Add course topics e.g. Mathematics, Physics, Chemistry"
+                    name='topics'
+                    onChange={handleChange}
+                />
+            </div>
+            <div className="mb-4">
+                <label className="block text-black text-sm font-semibold mb-2">Start date</label>
+                <input
+                    className="w-full p-2 text-black border rounded"
+                    type="date"
+                    name='startDate'
+                    onChange={handleChange}
+                />
+            </div>
+            <div className="mb-4">
+                <label className="block text-black text-sm font-semibold mb-2">End date</label>
+                <input
+                    className="w-full p-2 text-black border rounded"
+                    type="date"
+                    placeholder="Add course topics"
+                    name='endDate'
+                    onChange={handleChange}
+                />
+            </div>
+            <button
+                className="w-full bg-orange-600 text-white p-2 rounded hover:bg-orange-600 focus:outline-none focus:ring focus:border-orange-700"
+                type="submit">
+                Create
+            </button>
+        </form>
+    )
+}
+
+export default CreateCourse;
