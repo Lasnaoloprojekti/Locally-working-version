59b299634a177a2bc90392b11199f15322feaec4
JJ <johannes.jokinen@metropolia.fi>
2023-12-10 16:36:23 +0200

    prod

--- StudentServer/index.js
+++ StudentServer/index.js
@@ -18,7 +18,7 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: `https://student.northeurope.cloudapp.azure.com:3002`,
+    origin: `https://localhost:3002`,
     methods: ["GET", "POST"],
   },
 });
@@ -28,7 +28,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: `https://student.northeurope.cloudapp.azure.com:3002`,
+    origin: `https://localhost:3002`,
     methods: ["GET", "POST", "DELETE"],
     credentials: true,
   })
