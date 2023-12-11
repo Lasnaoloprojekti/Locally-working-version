8c542f151843957553e1a4e358ba3d4167d86ed4
JJ <johannes.jokinen@metropolia.fi>
2023-12-11 20:52:36 +0200

    prod test

--- Server/index.js
+++ Server/index.js
@@ -14,7 +14,6 @@ const {
 } = require("./models/collectionSchemas");
 const jwt = require("jsonwebtoken");
 const { Server } = require("socket.io");
-const io = new Server(server, { cors: corsOptions });
 const fetch = require("node-fetch");
 const multer = require("multer");
 const xlsx = require("xlsx");
@@ -37,6 +36,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 mongoose.connect(process.env.MONGODB_URI);
+
+const io = new Server(server, { cors: corsOptions });
 
 io.on("connection", (socket) => {
   console.log("a user connected", socket.id);
