7457692d13ce11338c4c8589abb9489bead4edf3
JJ <johannes.jokinen@metropolia.fi>
2023-12-13 20:22:44 +0200

    prod

--- ClassAttendanceTracker/src/View/AttendanceCollect.jsx
+++ ClassAttendanceTracker/src/View/AttendanceCollect.jsx
@@ -8,7 +8,7 @@ import { deleteSession } from "../Hooks/ApiHooks";
 import { v4 as uuid } from "uuid";
 
 const socket = io(process.env.S_CORS_ORIGIN, {
-  path: "/api",
+  path: "/socket.io",
 });
 
 export const WaitingPage = () => {
